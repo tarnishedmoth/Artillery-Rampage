61c3aa22fed4187c03194cfb1d990682676d7e81
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2025-03-01 00:40:17 -0500

    Main menu skeleton

---
+++ weapons/weapon_deployable.gd
@@ -0,0 +1,30 @@
+extends Weapon
+
+#region-- signals
+#endregion
+
+
+#region--Variables
+# statics
+# Enums
+# constants
+# @exports
+# public
+# _private
+# @onready
+#endregion
+
+
+#region--Virtuals
+func _init() -> void: pass
+func _enter_tree() -> void: pass
+func _ready() -> void: pass
+func _input(event: InputEvent) -> void: pass
+func _unhandled_input(event: InputEvent) -> void: pass
+func _physics_process(delta: float) -> void: pass
+func _process(delta: float) -> void: pass
+#endregion
+#region--Public Methodsfunc 
+#endregion
+#region--Private Methodsfunc _
+#endregion
