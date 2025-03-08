21394ec9a34d645390cc8ac6b179d932cca4a3cc
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2025-03-08 18:14:32 -0500

    Add health bar to house
    Health bar only appears when house is damaged

--- buildings/house.gd
+++ buildings/house.gd
@@ -4,6 +4,9 @@ extends Node
 
 @onready var health_label = $HealthLabel
 
+@onready var health_bar = $HouseHealthbar
+@onready var health_bar_current_health = $HouseHealthbar/GreenPart
+
 var health: float
 
 func _ready() -> void:
@@ -22,6 +25,8 @@ func take_damage(instigatorController: Node2D, instigator: Node2D, amount: float
 	print("House " + get_parent().name + " took " + str(actual_damage) + " damage; health=" + str(health))
 	
 	health_label.text = str(health)
+	health_bar.visible = true
+	health_bar_current_health.scale.x = health / max_health
 	
 	if health <= 0:
 		queue_free()
