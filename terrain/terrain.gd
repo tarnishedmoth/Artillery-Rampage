069495f71c864d7329dd6cfe0f29349eeffa5a29
Justin Montgomery <tryfinsigabrt@yahoo.com>
2025-03-09 15:04:49 -0500

    Attempt to solve projectile hit on terrain not damaging terrain
    - Debug case not triggered and terrain still doesn't get deformed in some cases - apparent with lead ball burst weapon
    - Issue might not be the scaled damage polygon itself but the clipping with the terrain itself so may need to take the iterative approach after trying the clipping

--- terrain/terrain.gd
+++ terrain/terrain.gd
@@ -14,6 +14,9 @@ const TerrainChunkScene = preload("res://terrain/terrain_chunk.tscn")
 @export_category("Projectile")
 @export var max_damage_shape_offset: float = 1
 
+@export_category("Projectile")
+@export var damage_scale_failure_reduction_step: float = 10.0
+
 @export_category("Crushing")
 @export var max_overlap_distance: float = 5
 
@@ -98,17 +101,39 @@ func _get_projectile_poly_global(projectile_poly: CollisionPolygon2D, poly_scale
 	return projectile_poly_global
 
 func _randomize_damage_polygon(projectile_damage_global: PackedVector2Array, poly_scale: Vector2) -> void:
-	var poly_scale_size:= poly_scale.length()
+	var poly_scale_size: float = poly_scale.length()
 	
-	for i in range(0, projectile_damage_global.size()):
-		projectile_damage_global[i] += _get_offset_damage_poly_vertex(poly_scale_size)
-			
-func _get_offset_damage_poly_vertex(scale: float) -> Vector2:
-	# TODO: This sometimes creates a non-viable polygon
-	return Vector2(0, _get_random_damage_offset(scale))
+	var projectile_damage_test_polygon: PackedVector2Array
+	projectile_damage_test_polygon.resize(projectile_damage_global.size())
+
+	var scale_decrements: float = poly_scale_size / damage_scale_failure_reduction_step
+	var success:bool = false
+
+	var scale_value: float = poly_scale_size
+	while scale_value > 0:
+		for i in range(0, projectile_damage_global.size()):
+			projectile_damage_test_polygon[i] = projectile_damage_global[i] + _get_offset_damage_poly_vertex(scale_value)
+		
+		if TerrainUtils._is_visible_polygon(projectile_damage_test_polygon):
+			success = true
+			break
+		else:
+			scale_value -= scale_decrements
+			print_debug("%s: Created non-viable damage result with damage polygon of size %d, trying again and reducing scale to %f" % [name, projectile_damage_test_polygon.size(), scale])
+
+	if success:
+		# copy the test polygon changes over to the original
+		for i in range(0, projectile_damage_global.size()):
+			projectile_damage_global[i] = projectile_damage_test_polygon[i]
+	else:
+		push_warning("%s: Unable to create a viable damage polygon after %d attempts - removing randomization" % [name, ceili(poly_scale_size / scale_decrements)])
+		# Will use the default polygon vertices passed in that are already scaled
+
+func _get_offset_damage_poly_vertex(scale_value: float) -> Vector2:
+	return Vector2(0, _get_random_damage_offset(scale_value))
 	
-func _get_random_damage_offset(scale: float) -> float:
-	return randf_range(min_damage_shape_offset * scale, max_damage_shape_offset * scale) * (1 if randf() >= 0.5 else -1)
+func _get_random_damage_offset(scale_value: float) -> float:
+	return randf_range(min_damage_shape_offset * scale_value, max_damage_shape_offset * scale_value) * (1 if randf() >= 0.5 else -1)
 			
 func _add_new_chunks(first_chunk: TerrainChunk,
  geometry_results: Array[PackedVector2Array], start_index: int) -> void:
