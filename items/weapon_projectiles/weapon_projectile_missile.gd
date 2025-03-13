aed622f36d44b182e25d41bc1aa77c95aec3dd42
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2025-03-13 01:29:10 -0400

    "Hellfire" downward firing & Homing missiles; various weapon tweaks

--- items/weapon_projectiles/weapon_projectile_missile.gd
+++ items/weapon_projectiles/weapon_projectile_missile.gd
@@ -1,9 +1,15 @@
 extends WeaponProjectile
 
+enum MissileBehaviorType {
+	Downward,
+	Homing
+}
+
+@export var behavior:MissileBehaviorType = MissileBehaviorType.Downward
 @export var thrust_sfx:AudioStreamPlayer2D
 @export var max_thrust:float = 1500.0
-@export var delay_before_targeting:float = 0.6
-@export var delay_before_thrusting:float = 0.8
+@export var delay_before_targeting:float = 0.5
+@export var delay_before_thrusting:float = 0.68
 @export var thrust_decay_time:float = 1.5
 @export var show_debug_targets:bool = false
 @onready var debug_target = $Debug_Target
@@ -27,20 +33,15 @@ func _ready() -> void:
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	#super()
 	if not _thrusting: return
+	var force: Vector2
 	var vector = Vector2.RIGHT.rotated(thruster.global_rotation)
-	var force = vector * _thrust * max_thrust
+	force = vector * _thrust * max_thrust
 	apply_central_force(force)
 	_last_force = force
 	
 func _physics_process(delta: float) -> void:
 	if _targeting:
-		var distance = global_position.distance_to(target)
-		var gravity_compensated_target = target + Vector2.UP * sqrt(distance) * 2 # This is very wrong but I am not good at this math
-		var intermediate = global_position+lerp(global_transform.x, global_position.direction_to(gravity_compensated_target),0.15)
-		if show_debug_targets:
-			debug_target.global_position = gravity_compensated_target
-			debug_target2.global_position = intermediate
-		look_at(intermediate)
+		targeting()
 
 func delay_targeting(time:float = delay_before_targeting) -> void:
 	await get_tree().create_timer(time).timeout
@@ -54,6 +55,23 @@ func delay_thrusting(time:float = delay_before_thrusting) -> void:
 	await get_tree().create_timer(thrust_decay_time).timeout
 	_thrusting = false
 	if thrust_sfx: thrust_sfx.stop()
+	
+func targeting() -> void:
+	var desired_target
+	var final_target
+	match behavior:
+		MissileBehaviorType.Downward:
+			desired_target = Vector2(global_position.x, global_position.y + 1.0)
+		MissileBehaviorType.Homing:
+			var distance = global_position.distance_to(target)
+			var gravity_compensated_target = target + Vector2.UP * sqrt(distance) * 2 # This is very wrong but I am not good at this math
+			desired_target = gravity_compensated_target
+			
+	final_target = global_position+lerp(global_transform.x, global_position.direction_to(desired_target),0.15)
+	if show_debug_targets:
+		debug_target.global_position = final_target
+		debug_target2.global_position = desired_target
+	look_at(final_target)
 
 func _find_nearest_target() -> Vector2:
 	var targets:Array
