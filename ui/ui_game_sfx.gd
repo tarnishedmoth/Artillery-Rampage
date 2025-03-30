ac5b7fe932b70eb960bcabdbb76cc11ad49b094d
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2025-03-29 20:51:29 -0400

    Added snowy mountain test level; snowy rock terrain texture; music in ui_game_sfx;

--- ui/ui_game_sfx.gd
+++ ui/ui_game_sfx.gd
@@ -4,15 +4,19 @@ extends Node
 ## Should handle things that are not positional in nature,
 ## like round end, turn change, game over...
 
+@export var soundtrack_file: AudioStream
+
 @export_group("AudioStreamPlayers","sfx_")
 @export var sfx_round_started: AudioStreamPlayer
 @export var sfx_round_ended: AudioStreamPlayer
 @export var sfx_turn_started: AudioStreamPlayer
 @export var sfx_turn_ended: AudioStreamPlayer
 @export var sfx_weapon_updated: AudioStreamPlayer
+@export var sfx_soundtrack: AudioStreamPlayer
 
 var initial_db_values:Dictionary
-var _volume_modifier:float = 1.0 ## This needs to be with other Options in a singleton so it can be requested.
+var _sfx_volume_modifier:float = 1.0 ## This needs to be with other Options in a singleton so it can be requested.
+var _music_volume_modifier:float = 0.5 ## This needs to be with other Options in a singleton so it can be requested.
 
 func _ready() -> void:
 	GameEvents.turn_started.connect(_on_turn_started)
@@ -22,7 +26,7 @@ func _ready() -> void:
 	GameEvents.weapon_updated.connect(_on_weapon_updated)
 	
 @warning_ignore("unused_parameter")
-func check_cached_volume(audio_stream_player, override_volume_db:float = _volume_modifier) -> void:
+func check_cached_volume(audio_stream_player, override_volume_db:float = _sfx_volume_modifier) -> void:
 	if not audio_stream_player in initial_db_values:
 		initial_db_values[audio_stream_player] = audio_stream_player.get_volume_db()
 		
@@ -30,36 +34,46 @@ func get_cached_volume(audio_stream_player) -> float:
 	if audio_stream_player in initial_db_values:
 		return initial_db_values[audio_stream_player]
 	else:
-		return _volume_modifier
+		return _sfx_volume_modifier
 		
-func get_volume_setting() -> float:
-	var nonlinear_volume = _volume_modifier
-	return nonlinear_volume # decibels
+func get_sfx_volume_setting() -> float:
+	var nonlinear_volume = linear_to_db(_sfx_volume_modifier)
+	return nonlinear_volume
+	
+func get_music_volume_setting() -> float:
+	var nonlinear_volume = linear_to_db(_music_volume_modifier)
+	return nonlinear_volume
 	
 func _on_turn_started(_player) -> void:
 	if sfx_turn_started:
-		sfx_turn_started.set_volume_db(get_volume_setting())
+		sfx_turn_started.set_volume_db(get_sfx_volume_setting())
 		sfx_turn_started.play()
 	
 func _on_turn_ended(_player) -> void:
 	if sfx_turn_ended:
-		sfx_turn_ended.set_volume_db(get_volume_setting())
+		sfx_turn_ended.set_volume_db(get_sfx_volume_setting())
 		sfx_turn_ended.play()
 
 func _on_round_started() -> void:
 	if sfx_round_started:
-		sfx_round_started.set_volume_db(get_volume_setting())
+		sfx_round_started.set_volume_db(get_sfx_volume_setting())
 		sfx_round_started.play()
+	if sfx_soundtrack:
+		sfx_soundtrack.stream = soundtrack_file
+		sfx_soundtrack.set_volume_db(get_music_volume_setting())
+		sfx_soundtrack.play()
 	
 func _on_round_ended() -> void:
 	if sfx_round_ended:
-		sfx_round_ended.set_volume_db(get_volume_setting())
+		sfx_round_ended.set_volume_db(get_sfx_volume_setting())
 		sfx_round_ended.play()
+	if sfx_soundtrack:
+		sfx_soundtrack.stop()
 	
 func _on_weapon_updated(_weapon) -> void:
 	# Weapons have their own equip SFX that we could leverage,
 	# but if you want one sound for all weapon changes just as an
 	# alert, this is the way.
 	if sfx_weapon_updated:
-		sfx_weapon_updated.set_volume_db(get_volume_setting())
+		sfx_weapon_updated.set_volume_db(get_sfx_volume_setting())
 		sfx_weapon_updated.play()
