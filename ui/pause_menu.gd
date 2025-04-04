252613612a21cb5073c7b1aa1f8ddc0ee7380ae7
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2025-04-03 20:10:55 -0400

    Options menu available in pause menu

--- ui/pause_menu.gd
+++ ui/pause_menu.gd
@@ -2,8 +2,11 @@ extends Control
 
 var paused = false;
 
+@onready var options_menu: PanelContainer = %OptionsMenu
+@onready var pause_menu: VBoxContainer = %PauseMenu
+
 func _ready():
-	self.hide()
+	hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 @warning_ignore("unused_parameter")
@@ -30,3 +33,11 @@ func _on_main_menu_pressed() -> void:
 
 func _on_quit_to_desktop_pressed() -> void:
 	get_tree().quit()
+
+func _on_options_pressed() -> void:
+	pause_menu.hide()
+	options_menu.show()
+
+func _on_options_menu_closed() -> void:
+	pause_menu.show()
+	options_menu.hide()
