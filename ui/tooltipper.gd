ee2c86bed94af4116fc3dd67c1f1ef9fa545d19c
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2025-04-03 23:16:23 -0400

    Contextual tooltips/hints

--- ui/tooltipper.gd
+++ ui/tooltipper.gd
@@ -1,14 +1,63 @@
 extends Control
 
+enum Context {
+	PlayerTurn,
+	EnemyTurn,
+}
+
+@export var transition_time:float = 1.2
+var current_context
+var is_player_turn:bool = false
+
+@onready var tooltips_player_turn: MarginContainer = %TooltipsPlayerTurn
+@onready var tooltips_enemy_turn: MarginContainer = %TooltipsEnemyTurn
+
+@onready var contexts: Array = [
+	tooltips_player_turn,
+	tooltips_enemy_turn,
+]
+
 func _ready() -> void:
+	GameEvents.turn_started.connect(_on_turn_started)
+	
 	GameEvents.user_options_changed.connect(_on_user_options_changed)
-	if UserOptions.show_tooltips:
+	toggle_visibility(true)
+	
+	
+func toggle_visibility(toggle:bool = true) -> void:
+	if UserOptions.show_tooltips and toggle == true:
 		show()
-	else:
-		hide()
+		return
+			
+	if visible: hide()
+	
+func switch_display_to_new_context(context: Context) -> void:
+	if context == current_context: return
+	current_context = context
+	
+	for ui in contexts:
+		ui.hide()
+		
+	await transition() # Wait
+	
+	match context:
+		Context.PlayerTurn:
+			tooltips_player_turn.show()
+		Context.EnemyTurn:
+			tooltips_enemy_turn.show()
+			
+func transition(time:float = transition_time) -> bool:
+	await get_tree().create_timer(time).timeout
+	return true
+
 
 func _on_user_options_changed() -> void:
-	if UserOptions.show_tooltips:
-		if not visible: show()
+	toggle_visibility()
+
+func _on_turn_started(controller: TankController) -> void:
+	if controller is not Player:
+		is_player_turn = false
+		switch_display_to_new_context(Context.EnemyTurn)
 	else:
-		if visible: hide()
+		is_player_turn = true
+		switch_display_to_new_context(Context.PlayerTurn)
