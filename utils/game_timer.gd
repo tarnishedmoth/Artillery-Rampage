212fc0c84fbeddc605873ca26ec8a209d9417d04
Justin Montgomery <tryfinsigabrt@yahoo.com>
2025-04-14 18:24:32 -0500

    Add a frame counter to game_timer

--- utils/game_timer.gd
+++ utils/game_timer.gd
@@ -1,6 +1,7 @@
 class_name GameTimer extends Node
 
 var _time_seconds:float = 0.0  # Elapsed game time in seconds
+var _frame_count:int = 0
 
 func _init() -> void:
 	process_mode = ProcessMode.PROCESS_MODE_PAUSABLE
@@ -13,7 +14,11 @@ var time_ms: float:
 	get:
 		return _time_seconds * 1000
 
+var frame:int:
+	get: return _frame_count
+	
 func _process(delta: float) -> void:
 	_time_seconds += delta
+	_frame_count += 1
+	
 	RenderingServer.global_shader_parameter_set(&"game_time", _time_seconds)
-	# print_debug("TimeSeconds=%fs; EngineTime=%fs" % [_time_seconds, Time.get_ticks_msec() / 1000.0])
