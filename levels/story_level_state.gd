3bb2940b4819fc387093716e6a930f4273dea57a
Justin Montgomery <tryfinsigabrt@yahoo.com>
2025-04-19 19:13:53 -0500

    Add additional logging around scene changing
    - Identified bug as story level does not advance if you die and then win in a subsequent attempt

--- levels/story_level_state.gd
+++ levels/story_level_state.gd
@@ -9,8 +9,6 @@ func _ready() -> void:
 		push_error("Parent=%s is not a GameLevel" % [get_parent().name if get_parent() else &"NULL"])
 		return
 		
-	#restore_from_save_state(SaveStateManager.save_state)
-	
 	GameEvents.round_ended.connect(_on_round_ended)
 	
 func _on_round_ended() -> void:
@@ -18,9 +16,11 @@ func _on_round_ended() -> void:
 	var player_won:bool = not RoundStatTracker.round_data.died
 	print_debug("On Round ended: Player won=%s" % player_won)
 	
-	# Only increment level if won
+	# Only increment level if won.
+	# SceneManager.next_level increments so "current_level_index" is actually the next level here
 	if player_won:
 		_last_completed_level = SceneManager._current_level_index
+		print_debug("last_completed_level=%d" [_last_completed_level])
 		# TODO: This can increment beyond the end which will invalidate the story state
 	
 #region Savable
@@ -35,6 +35,7 @@ func restore_from_save_state(save: SaveState) -> void:
 	var level:int = restore_story_level_state(save)
 	if level != -1:
 		_last_completed_level = level
+		print_debug("last_completed_level=%d" [_last_completed_level])
 
 func update_save_state(save:SaveState) -> void:
 	if not save or not save.state:
@@ -43,7 +44,7 @@ func update_save_state(save:SaveState) -> void:
 	if _last_completed_level >= 0:
 		var state:Dictionary = save.state.get_or_add(SAVE_STATE_KEY, {})
 		state[_LEVEL_KEY] = _last_completed_level
-
+		print_debug("set save state last completed level=%d" [_last_completed_level])
 	
 static func restore_story_level_state(save: SaveState) -> int:
 	if not save or not save.state:
@@ -56,6 +57,7 @@ static func restore_story_level_state(save: SaveState) -> int:
 	var state:Dictionary = save.state[SAVE_STATE_KEY]
 
 	var last_level:int = state.get(_LEVEL_KEY, 0)
+	print_debug("set story level index=%d" [last_level])
 	SceneManager.set_story_level_index(last_level)
 	
 	return last_level
--- utils/scene_manager.gd
+++ utils/scene_manager.gd
@@ -88,14 +88,17 @@ func next_level(delay: float = default_delay) -> void:
 	# TODO: When using procedural maps may need a different strategy or may want to shuffle the levels on start
 	# The procedural map could just be a specific scene that has some base configuration and then generates on ready
 	# Or we could start proc-gening the next scene during current scene and then just keep in memory and present it here
-	print_debug("Loading")
 	_current_story_level = story_levels.levels[_current_level_index]
+	print_debug("Loading story level index=%d -> %s" % [_current_level_index, _current_story_level.name])
+
 	await switch_scene_file(_current_story_level.scene_res_path, delay)
 	
 	_current_level_index = (_current_level_index + 1) % story_levels.levels.size()
+	print_debug("set _current_level_index=%d" % [_current_level_index])
 
 func set_story_level_index(index:int) -> bool:
 	if index >= 0 and index < story_levels.levels.size():
+		print_debug("set story level index=%d" % [index])
 		_current_level_index = index
 		return true
 	push_warning("Attempted to set invalid story level index=%d - expected [0, %d)" % [index, story_levels.levels.size()])
@@ -191,6 +194,8 @@ func _switch_scene(switchFunc: Callable, delay: float) -> void:
 	get_tree().root.add_child(current_scene)
 	get_tree().current_scene = current_scene
 	
+	#await get_tree().process_frame
+
 	SaveStateManager.restore_tree_state()
 	
 	get_tree().paused = false
