fefc313bafe741eb13a7b8db3f9d3c83bb940625
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2025-05-03 10:46:34 -0400

    Enabled trajectory preview by default to get feedback

--- utils/user_options.gd
+++ utils/user_options.gd
@@ -4,7 +4,7 @@ var keybinds:Dictionary[StringName,InputEvent] # action, keybind
 
 var show_tooltips:bool = true
 var show_hud:bool = true
-var show_assist_trajectory_preview:bool = false
+var show_assist_trajectory_preview:bool = true
 
 var volume_music:float = 0.8
 var volume_sfx:float = 1.0
