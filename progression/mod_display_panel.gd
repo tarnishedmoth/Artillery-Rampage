5238ab3246828047479f0995d69bb4dccb284c11
Justin Montgomery <tryfinsigabrt@yahoo.com>
2025-06-25 14:58:59 -0500

    Fix not in scene tree error that sometimes appears in logs when upgrade screen is freed in scene switch
    - Occurred on the mouse exited event when attempting to call hide_timer.start
    - De-registering the mouse events on tree exited to pair with tree entered so that they don't fire spuriously

--- progression/mod_display_panel.gd
+++ progression/mod_display_panel.gd
@@ -28,6 +28,10 @@ func _enter_tree() -> void:
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
 
+func _exit_tree() -> void:
+	mouse_entered.disconnect(_on_mouse_entered)
+	mouse_exited.disconnect(_on_mouse_exited)
+	
 func _ready() -> void:
 	buttons.hide()
 	delete.show()
@@ -93,7 +97,6 @@ func _on_mouse_entered() -> void:
 	
 func _on_mouse_exited() -> void:
 	hide_timer.start()
-
 
 func _on_delete_pressed() -> void:
 	toggle_buttons(true)
