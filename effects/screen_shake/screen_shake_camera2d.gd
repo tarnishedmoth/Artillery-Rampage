77782f3e381ee9481b2fc58d4c8bf4ecfa699921
Justin Montgomery <tryfinsigabrt@yahoo.com>
2025-07-01 23:25:01 -0500

    Add screen shake effect to GameLevel
    - Add a camera 2D extension for a screen shake camera based on https://kidscancode.org/godot_recipes/4.x/2d/screen_shake/index.html
    - Add a node child that listens for projectile events and adds trauma to the ScreenShakeCamera2D based on the max_damage from a curve

---
+++ effects/screen_shake/screen_shake_camera2d.gd
@@ -0,0 +1,50 @@
+## Adapted from https://kidscancode.org/godot_recipes/4.x/2d/screen_shake/index.html
+class_name ScreenShakeCamera2D extends Camera2D
+
+@export var decay: float = 0.8
+@export var max_offset: Vector2= Vector2(10, 100)
+@export var max_roll: float = 0.05
+@export var trauma_power: float = 2.0
+
+@export var noise_template : FastNoiseLite
+@export var randomize_seed:bool = true
+var _noise:FastNoiseLite
+
+var trauma: float = 0.0
+var noise_y: float= 0.0
+
+func _ready():
+	_noise = _generate_noise()
+	make_current()
+
+func add_trauma(amount: float):
+	trauma = clampf(trauma + amount, 0.0, 1.0)
+
+func _process(delta):
+	if trauma > 0.0:
+		trauma = maxf(trauma - decay * delta, 0.0)
+		shake()
+
+func shake():
+	var amt:float = pow(trauma, trauma_power)
+	noise_y += 1.0
+	
+	offset.x = max_offset.x * amt * _noise.get_noise_2d(_noise.seed * 2, noise_y)
+	offset.y = max_offset.y * amt * _noise.get_noise_2d(_noise.seed * 3, noise_y)
+	rotation = max_roll * amt * _noise.get_noise_2d(_noise.seed, noise_y)
+
+func _generate_noise() -> FastNoiseLite:
+	var noise : FastNoiseLite
+	
+	if noise_template:
+		noise = noise_template.duplicate()
+	else:
+		noise = FastNoiseLite.new()
+		noise.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
+		
+	if randomize_seed:
+		noise.seed = randi()
+	
+	print_debug("%s - Using seed=%d for noise" % [name, noise.seed])
+	
+	return noise
---
+++ effects/screen_shake/weapon_projectile_shaker.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@export var trauma_v_max_damage:Curve
+@export var screen_shake_camera:ScreenShakeCamera2D
+
+func _ready() -> void:
+	if not screen_shake_camera:
+		push_error("%s: Missing screen_shake_camera assignment" % name)
+		return
+	if not trauma_v_max_damage:
+		push_error("%s: Missing trauma_v_max_damage" % name)
+		return
+		
+	GameEvents.projectile_fired.connect(_on_projectile_fired)
+	
+func _on_projectile_fired(projectile: WeaponProjectile) -> void:
+	if OS.is_debug_build():
+		print_debug("%s: projectile %s fired" % [name, projectile.name])
+	projectile.completed_lifespan.connect(_on_projectile_exploded)
+
+func _on_projectile_exploded(projectile: WeaponProjectile) -> void:
+	var new_trauma:float = trauma_v_max_damage.sample(projectile.max_damage)
+	screen_shake_camera.add_trauma(new_trauma)
+	
+	if OS.is_debug_build():
+		print_debug("%s: projectile %s exploded - max_damage=%f; trauma_added=%f; total_trauma=%f" % 
+			[name, projectile.name, projectile.max_damage, new_trauma, screen_shake_camera.trauma])
+	
