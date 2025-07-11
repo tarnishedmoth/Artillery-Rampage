88d84c261545bd85dfc634e8e6de78ca56347a78
Justin Montgomery <tryfinsigabrt@yahoo.com>
2025-07-10 22:21:03 -0500

    Fix damaged terrain outline not applying modulate values correctly for day/night cycle

--- terrain/terrain_chunk_texture_outline.gd
+++ terrain/terrain_chunk_texture_outline.gd
@@ -25,16 +25,16 @@ func apply_to_outline(line: Line2D, impact_vertices: PackedVector2Array) -> void
 	var shader_material = material as ShaderMaterial
 	if shader_material:
 		var discarded_points:PackedVector2Array = _compute_discarded_vertices(line, impact_vertices)
-		
+				
 		shader_material.set_shader_parameter("discarded_vertices", discarded_points)
 		shader_material.set_shader_parameter("discarded_count", discarded_points.size())
 		shader_material.set_shader_parameter("discard_match_distance", line.width * outline_mesh_outline_shader_fraction)
+		shader_material.set_shader_parameter("modulate", ObjectUtils.get_effective_modulate(line))
 		
 	line.material = material
 	line.set_texture(texture)
 	line.texture_repeat = repeat
 	# Line2D doesn't support texture_offset
-
 
 func _compute_discarded_vertices(line: Line2D, impact_vertices: PackedVector2Array)  -> PackedVector2Array:
 	var discarded_points:PackedVector2Array = []
--- utils/object_utils.gd
+++ utils/object_utils.gd
@@ -6,3 +6,12 @@ func wait_free(node: Node) -> void:
 	# if that call is itself deferred we may need to wait multiple frames
 	while is_instance_valid(node):
 		await get_tree().process_frame
+
+## Computes effective modulate value that will be applied to this node at rendering time
+## Multiplies all the self_modulate and modulate values up to its top-most canvas item parent
+func get_effective_modulate(node: CanvasItem) -> Color:
+	var result: Color = Color.WHITE
+	while node:
+		result *= node.self_modulate * node.modulate
+		node = node.get_parent() as CanvasItem
+	return result
