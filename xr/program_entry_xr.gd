8460026de14b1f1563d693fdf1382a7ed618a2bb
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2025-07-23 02:45:55 -0400

    Launch using more XRTools components

--- xr/program_entry_xr.gd
+++ xr/program_entry_xr.gd
@@ -38,18 +38,18 @@ var xr_interface: XRInterface
 @export var input_gamepad : bool = false
 
 func _ready():
-	xr_interface = XRServer.find_interface("OpenXR")
-	if xr_interface and xr_interface.is_initialized():
-		print("OpenXR initialized successfully")
-
-		# Turn off v-sync!
-		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
-
-		# Change our main viewport to output to the HMD
-		get_viewport().use_xr = true
-	else:
-		print("OpenXR not initialized, please check if your headset is connected")
-		
+	#xr_interface = XRServer.find_interface("OpenXR")
+	#if xr_interface and xr_interface.is_initialized():
+		#print("OpenXR initialized successfully")
+#
+		## Turn off v-sync!
+		#DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+#
+		## Change our main viewport to output to the HMD
+		#get_viewport().use_xr = true
+	#else:
+		#print("OpenXR not initialized, please check if your headset is connected")
+		#
 	set_viewport(InternalSceneRoot)
 	SceneManager.switch_scene(program_entry_2d)
 	
